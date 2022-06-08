e5fa742b46a2bc062f3b8f5131fd8682abe6b179
Mohammad Zamani <zamani.md@gmail.com>
2022-06-08 12:11:13 -0600

    Add Models

---
+++ MVCCore6/Models/Service.cs
@@ -0,0 +1,18 @@
+﻿namespace MVCCore6.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+
+        public Service(int id, string name, string description, string imageName)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ImageName = imageName;
+        }
+    }
+}
---
+++ MVCCore6/Models/Slider.cs
@@ -0,0 +1,20 @@
+﻿namespace MVCCore6.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string LinkText { get; set; }
+        public string ImageName { get; set; }
+
+        public Slider(int id, string name, string description, string linkText, string imageName)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            LinkText = linkText;
+            ImageName = imageName;
+        }
+    }
+}
